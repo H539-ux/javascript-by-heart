8fed33dc3a26db1c351dc66665bd9e7848555a04
Harshal <harshalrajput312@gmail.com>
2025-05-13 16:23:36 -0700

    added throttling

---
+++ Debouncing an throttling/throttling.js
@@ -0,0 +1,25 @@
+  const input = document.getElementById('search-box')
+  const output = document.getElementById('output')
+
+
+
+
+  const throttle = (func,delay)=>{
+    let timer = 0;
+
+    return function(...args){
+      const current =  Date.now();
+        if(current-timer>=delay || timer ==0){
+          console.log(`${current} - ${timer} = ${current-timer}`)
+          func.apply(this,args);
+          timer=current;
+        }
+    }
+  }
+
+
+ const search = (e)=>{
+  output.textContent = e.target.value;
+ }
+  const throttleFunction = throttle(search,500)
+  input.addEventListener('input',throttleFunction)
